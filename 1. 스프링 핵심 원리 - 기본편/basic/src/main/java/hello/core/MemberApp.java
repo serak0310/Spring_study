f7a21ed9a8237f7e331fc6c9c1dcf2cdf8df6a75
serak0310 <serak0310@naver.com>
2024-05-13 02:54:23 +0900

    03. 스프링 핵심 원리 이해2 - 객체 지향 원리 적용

--- 1. 스프링 핵심 원리 - 기본편/basic/src/main/java/hello/core/MemberApp.java
+++ 1. 스프링 핵심 원리 - 기본편/basic/src/main/java/hello/core/MemberApp.java
@@ -4,10 +4,17 @@ import hello.core.member.Grade;
 import hello.core.member.Member;
 import hello.core.member.MemberService;
 import hello.core.member.MemberServiceImpl;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class MemberApp {
     public static void main(String[] args) {
-        MemberService memberService = new MemberServiceImpl();
+//        AppConfig appConfig = new AppConfig();
+//        MemberService memberService = appConfig.memberService();
+
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
+        MemberService memberService = applicationContext.getBean("memberService", MemberService.class); // Config에서 꺼낼 빈 메서드 이름, 반환타입
+
         Member memberA = new Member(1L, "memberA", Grade.VIP);
         memberService.join(memberA);
 
